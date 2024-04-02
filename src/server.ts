ec5edc531e2a03901a26c0880452613a85f28486
Di0g0Pimenta <diogo2003.pimenta@gmail.com>
2024-04-02 11:16:05 +0100

    Create route events

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,32 @@
 import fastify from "fastify";
+import { date, z } from "zod";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
 
-app.get("/", () => {
-  return "Hello, World!";
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+
+app.post("/events", async (request, reply) => {
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+  });
+
+  const data = createEventSchema.parse(request.body);
+
+  const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      details: data.details,
+      maxAttendees: data.maximumAttendees,
+      slug: new Date().toISOString(),
+    },
+  });
+
+  return reply.status(201).send({ event: event.id });
 });
 
 app.listen({ port: 3333 }).then(() => {
