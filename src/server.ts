646bf68ccb66a71230260458162ea0cd7da0d211
Di0g0Pimenta <diogo2003.pimenta@gmail.com>
2024-04-02 15:17:12 +0100

    End of the Create Event route and organization of folders and code

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,16 @@
 import fastify from "fastify";
-import { date, z } from "zod";
-import { PrismaClient } from "@prisma/client";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event"
 
 const app = fastify();
 
-const prisma = new PrismaClient({
-  log: ["query"],
-});
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.post("/events", async (request, reply) => {
-  const createEventSchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximumAttendees: z.number().int().positive().nullable(),
-  });
-
-  const data = createEventSchema.parse(request.body);
-
-  const event = await prisma.event.create({
-    data: {
-      title: data.title,
-      details: data.details,
-      maxAttendees: data.maximumAttendees,
-      slug: new Date().toISOString(),
-    },
-  });
-
-  return reply.status(201).send({ event: event.id });
-});
+app.register(createEvent)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server Running!");
