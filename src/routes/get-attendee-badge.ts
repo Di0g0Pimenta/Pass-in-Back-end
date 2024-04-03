d503d543e2bb0097e7ce976cf4fb5ad28139ce51
Di0g0Pimenta <diogo2003.pimenta@gmail.com>
2024-04-03 14:35:04 +0100

    Create check ins table

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -11,7 +11,16 @@ export async function getAttendeeBadge(app: FastifyInstance) {
         params: z.object({
           attendeeId: z.coerce.number().int(),
         }),
-        response: {},
+        response: {
+          200: z.object({
+            badge: z.object({
+              name: z.string(),
+              email: z.string().email(),
+              eventTitle: z.string(),
+              checkInUrl: z.string().url(),
+            }),
+          }),
+        },
       },
     },
     async (request, reply) => {
@@ -36,7 +45,18 @@ export async function getAttendeeBadge(app: FastifyInstance) {
         throw new Error("Ateendee not found");
       }
 
-      return reply.send({ attendee });
+      const baseUrl = `${request.protocol}://${request.hostname}`;
+
+      const checkInUrl = new URL(`/attendees/${attendeeId}/check-in`, baseUrl);
+
+      return reply.send({
+        badge: {
+          name: attendee.name,
+          email: attendee.email,
+          eventTitle: attendee.event.title,
+          checkInUrl: checkInUrl.toString(),
+        },
+      });
     }
   );
 }
